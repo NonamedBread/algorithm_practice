9b216dfe4a7cecc25d5e59e277a5ce79ad0f7158
Lee KyuHwan <93482597+NonamedBread@users.noreply.github.com>
2024-02-09 19:11:06 +0900

    Update 곱셈.js

--- 백준/Bronze/2588. 곱셈/곱셈.js
+++ 백준/Bronze/2588. 곱셈/곱셈.js
@@ -22,17 +22,9 @@ rl
 const solution = (input) => {
 
   const [n, m] = input;
-  let result = 0;
-  const mArray = Array.from(String(m), Number).reverse().map((el) => {
+  Array.from(String(m), Number).reverse().map((el) => {
     console.log(el * n)
-
-    return el * n
   });
 
-  mArray.forEach((el, idx) => {
-    result += el * Math.pow(10, idx);
-  })
-
-  console.log(result)
-
+  console.log(n * m)
 }
