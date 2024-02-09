1c96e8c32174f8814a3724fbfca0b9c4b86536f6
Lee KyuHwan <93482597+NonamedBread@users.noreply.github.com>
2024-02-09 14:21:53 +0900

    [Bronze V] Title: A/B, Time: 144 ms, Memory: 9712 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.js
@@ -0,0 +1,28 @@
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+let list = [];
+
+rl
+  .on("line", function (line) {
+    input.push(line);
+  })
+  .on("close", function () {
+    input.forEach((val) => {
+      list.push(val.split(" ").map((el) => parseInt(el)));
+    });
+
+    list.map((el) => {
+      solution(el);
+    });
+    process.exit();
+  });
+
+const solution = (input) => {
+  const A = parseInt(input[0]);
+  const B = parseInt(input[1]);
+    console.log(A / B);
+}
