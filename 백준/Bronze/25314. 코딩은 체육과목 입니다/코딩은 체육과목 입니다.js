89ef4fcb27a86b01a0019d4eba3cc12fb073f813
Lee KyuHwan <93482597+NonamedBread@users.noreply.github.com>
2024-03-02 12:21:03 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 140 ms, Memory: 9456 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.js
@@ -0,0 +1,25 @@
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input.push(Number(line));
+}).on("close", function () {
+  solution(input);
+  process.exit();
+});
+
+const solution = (input) => {
+  const count = input[0] / 4;
+  let sol = "";
+
+  for (let i = 1; i <= count; i++) {
+    sol += "long ";
+  }
+  sol += "int";
+
+  console.log(sol);
+};
