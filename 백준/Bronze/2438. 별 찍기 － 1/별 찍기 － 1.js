014a37c901db09f75ba8a84e9a3d7a2c3b55c4df
Lee KyuHwan <93482597+NonamedBread@users.noreply.github.com>
2024-04-03 12:52:49 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 148 ms, Memory: 9624 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -0,0 +1,18 @@
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input.push(Number(line));
+}).on("close", function () {
+  const length = input[0]
+  for (let i = 1; i <= length; i++) {
+    const star = "*".repeat(i)
+    console.log(star)
+  }
+
+  process.exit();
+});
