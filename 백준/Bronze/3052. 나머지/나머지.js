2dad566470e71ce657c9095ae9ae631cbc5d54b7
Lee KyuHwan <93482597+NonamedBread@users.noreply.github.com>
2024-04-18 06:33:46 +0900

    [Bronze II] Title: 나머지, Time: 148 ms, Memory: 9748 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.js
@@ -0,0 +1,13 @@
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on("line", (line) => {
+  input.push(line.split(" ").map(Number));
+}).on("close", () => {
+  const set = new Set(input.map((el) => el % 42));
+  console.log(set.size)
+});
