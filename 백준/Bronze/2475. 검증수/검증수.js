d65f7273139444704588102e646e704386780f6a
Lee KyuHwan <93482597+NonamedBread@users.noreply.github.com>
2024-04-20 14:53:28 +0900

    [Bronze V] Title: 검증수, Time: 152 ms, Memory: 9476 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.js
+++ 백준/Bronze/2475. 검증수/검증수.js
@@ -8,11 +8,7 @@ const input = [];
 rl.on("line", (line) => {
   input.push(line.split(" ").map(Number));
 }).on("close", () => {
-  let sum = 0;
-
-input[0].forEach((el) => {
-    sum += el * el;
-});
+  const sum = input[0].reduce((acc, cur) => acc + cur * cur, 0);
 
   console.log(sum % 10);
 });
