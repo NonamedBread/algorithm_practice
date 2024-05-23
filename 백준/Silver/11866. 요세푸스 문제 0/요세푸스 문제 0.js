70f38ba9050b54d017fac068069c5d6e6a97aea4
Lee KyuHwan <93482597+NonamedBread@users.noreply.github.com>
2024-05-23 09:57:48 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 148 ms, Memory: 9856 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.js
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.js
@@ -8,17 +8,18 @@ const input = [];
 rl.on("line", (line) => {
   input.push(line.split(" ").map((el) => Number(el)));
 }).on("close", () => {
-const length = input[0][0];
-const number = input[0][1];
+  const length = input[0][0];
+  const number = input[0][1];
 
-let arr = Array.from({length: length}, (v,i) => i + 1);
-let result = [];
+  let arr = Array.from({length: length}, (v,i) => i + 1);
+  let result = [];
+  let index = 0;
 
-while(arr.length > 0){
-  for(let i = 0; i < number - 1; i++){
-    arr.push(arr.shift());
+  while(arr.length > 0){
+    index = (index + number - 1) % arr.length;
+    result.push(arr[index]);
+    arr.splice(index, 1);
   }
-  result.push(arr.shift());
-}
-console.log(`<${result.join(', ')}>`);
+
+  console.log(`<${result.join(', ')}>`);
 });
