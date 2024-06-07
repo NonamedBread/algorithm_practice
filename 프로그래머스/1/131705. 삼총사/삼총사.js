8864dce1a3e5f883c46a047535391bd9d8e74f60
Lee KyuHwan <93482597+NonamedBread@users.noreply.github.com>
2024-06-07 17:45:51 +0900

    [level 1] Title: 삼총사, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -1,15 +1,16 @@
 function solution(number) {
-    let answer = 0;
-    const numLen = number.length;
-    for(let i = 0; i < numLen; i++){
-        for(let j = i + 1; j < numLen; j++){
-            for(let k = j + 1; k < numLen; k++){
-                if(number[i] + number[j] + number[k] === 0) {
-                  answer++;
-                }
-            }
+    let result = 0;
+
+    const combination = (current, start) => {
+        if (current.length === 3) {
+            result += current.reduce((acc, cur) => acc + cur, 0) === 0 ? 1 : 0;
+            return;
+        }
+
+        for (let i = start; i < number.length; i++) {
+            combination([...current, number[i]], i + 1);
         }
     }
-    
-    return answer;
+    combination([], 0);
+    return result;
 }
