8ef75920666c35cd28a6a1d825e2292bdac3d5c7
Lee KyuHwan <93482597+NonamedBread@users.noreply.github.com>
2024-09-26 21:08:29 +0900

    [Bronze III] Title: 직각삼각형, Time: 104 ms, Memory: 9576 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.js
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.js
@@ -8,9 +8,15 @@ const input = [];
 rl.on("line", (line) => {
   input.push(line.split(" ").map((el) => parseInt(el)));
 }).on("close", () => {
-  for (let i = 0; i < input.length -1; i++) {
-    let [a, b, c] = input[i].map((el) => el ** 2).sort((a, b) => a - b);
-    const result = a + b === c ? "right" : "wrong";
-    console.log(result);
+  for (let i = 0; i < input.length -1 ; i++) {
+    const triangle = input[i];
+    const sort = triangle.sort((a, b) => b - a);
+
+    if (sort[0] ** 2 === sort[1] ** 2 + sort[2] ** 2) {
+      console.log("right");
+    } else {
+      console.log("wrong");
+    }
+
   }
 });
